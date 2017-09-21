bbbffd3b88eacd2d3a93ac12cb307091fa1f09ee
izaak <izaak@mobcut.com>
2017-09-22 05:03:15 +0800

    Add buy button

--- src/templates/product.js
+++ src/templates/product.js
@@ -11,7 +11,17 @@ export default function Template({
     <div className="flex mw7 center avenir">
       <Helmet title={`${product.frontmatter.title}`} />
       <div className="blog-post">
-        <h1>{product.frontmatter.title}</h1>
+        <h1>
+          {product.frontmatter.title}{" "}
+          <div className="f6 fr grow no-underline br-pill ph3 pv2 mb0 dib white bg-hot-pink">
+            Buy for{" "}
+            {`RM` +
+              product.frontmatter.price.toString().slice(0, -2) +
+              "." +
+              product.frontmatter.price.toString().slice(-2)}
+          </div>
+        </h1>
+
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: product.html }}
